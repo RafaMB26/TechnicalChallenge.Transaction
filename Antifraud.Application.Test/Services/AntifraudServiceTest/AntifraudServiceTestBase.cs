0120b63f6acf093ea0ff580cacfad972075a05fb
Rafael Moreno <rafael.brenes@modere.com>
2025-04-29 14:29:53 -0600

    Adding unit tests for the antifraud service

---
+++ Antifraud.Application.Test/Services/AntifraudServiceTest/AntifraudServiceTestBase.cs
@@ -0,0 +1,49 @@
+﻿using Antifraud.Application.Services;
+using Antifraud.Domain.Interfaces.Producer;
+using Antifraud.Domain.Interfaces.Repositories;
+using Common.DTOs;
+using Common.Result;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+
+namespace Antifraud.Application.Test.Services.AntifraudServiceTest;
+public class AntifraudServiceTestBase
+{
+    protected ILogger<AntifraudService> _loggerMock;
+    protected ITransactionRepository _transactionRepositoryMock;
+    protected IAntifraudProducer _antifraudProducerMock;
+    protected TransactionDTO _transactionToValidate;
+    protected AntifraudService _antifraudService;
+
+    [SetUp]
+    public void Setup()
+    {
+        _loggerMock = Substitute.For<ILogger<AntifraudService>>();
+        _transactionRepositoryMock = Substitute.For<ITransactionRepository>();
+        _antifraudProducerMock = Substitute.For<IAntifraudProducer>();
+
+        _antifraudService = new AntifraudService(_transactionRepositoryMock, _antifraudProducerMock, _loggerMock);
+
+        _transactionToValidate = new TransactionDTO()
+        {
+            TransactionExternalId = Guid.NewGuid(),
+            CreatedAt = DateTime.UtcNow,
+            SourceAccountId = Guid.NewGuid(),
+            TargetAccountId = Guid.NewGuid(),
+            TransferTypeId = 1,
+            Value = 200,
+            Status = 1,
+        };
+
+        var sumOfTheDayResult = new Result<decimal>(200);
+
+        _transactionRepositoryMock.GetSumOfTransactionsAsync(Arg.Any<Guid>())
+            .Returns(Task.FromResult(sumOfTheDayResult));
+
+        _transactionRepositoryMock.AddTransactionValueAsync(Arg.Any<Guid>(), 200)
+            .Returns(Task.FromResult(new Result<bool>(true)));
+
+        _antifraudProducerMock.ProduceAsync(Arg.Any<TransactionProcessedStatusDTO>())
+            .Returns(Task.FromResult(new Result<bool>(true)));
+    }
+}
---
+++ Antifraud.Application.Test/Services/AntifraudServiceTest/IsTransactionCorrectTest.cs
@@ -0,0 +1,89 @@
+﻿using Common.DTOs;
+using Common.Result;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+
+namespace Antifraud.Application.Test.Services.AntifraudServiceTest;
+
+[TestFixture]
+public class IsTransactionCorrectTest : AntifraudServiceTestBase
+{
+    [Test]
+    public async Task IsTransactionCorrect_TransactionIsCorrect_CallProducer()
+    {
+        var result = await _antifraudService.IsTransactionCorrectAsync(_transactionToValidate);
+
+        Assert.IsNotNull(result);
+        Assert.IsTrue(result.IsSuccess);
+        Assert.IsNotNull(result.Data);
+        Assert.IsTrue(result.Data.IsCorrect);
+
+        await _antifraudProducerMock.Received(1).ProduceAsync(Arg.Any<TransactionProcessedStatusDTO>());
+    }
+
+    [Test]
+    public async Task IsTransactionCorrect_TransactionRepositoryThrowsError_DoNotCallProducer()
+    {
+        var errorMessage = "Error while trying to get the sum of the customer";
+        _transactionRepositoryMock.GetSumOfTransactionsAsync(Arg.Any<Guid>())
+            .Returns(Task.FromResult(new Result<decimal>(new Error(errorMessage, 503))));
+
+        var result = await _antifraudService.IsTransactionCorrectAsync(_transactionToValidate);
+
+        Assert.IsNotNull(result);
+        Assert.IsFalse(result.IsSuccess);
+        Assert.IsNotNull(result.Error);
+        Assert.That(result.Error.Message, Is.EqualTo(errorMessage));
+        Assert.That(result.Error.Code, Is.EqualTo(503));
+
+        await _antifraudProducerMock.DidNotReceive().ProduceAsync(Arg.Any<TransactionProcessedStatusDTO>());
+    }
+
+
+    [Test]
+    public async Task IsTransactionCorrect_TransactionValueIsGraterThanTwoThousand_TransactionIsNotCorrect()
+    {
+        _transactionToValidate.Value = 2001;
+        var result = await _antifraudService.IsTransactionCorrectAsync(_transactionToValidate);
+
+        Assert.IsNotNull(result);
+        Assert.IsTrue(result.IsSuccess);
+        Assert.IsNotNull(result.Data);
+        Assert.IsFalse(result.Data.IsCorrect);
+
+        await _antifraudProducerMock.Received(1).ProduceAsync(Arg.Any<TransactionProcessedStatusDTO>());
+    }
+
+    [Test]
+    public async Task IsTransactionCorrect_SumOfTheDayIsGraterThanTwentyThousand_TransactionIsNotCorrect()
+    {
+        _transactionRepositoryMock.GetSumOfTransactionsAsync(Arg.Any<Guid>())
+            .Returns(Task.FromResult(new Result<decimal>(20001)));
+        var result = await _antifraudService.IsTransactionCorrectAsync(_transactionToValidate);
+
+        Assert.IsNotNull(result);
+        Assert.IsTrue(result.IsSuccess);
+        Assert.IsNotNull(result.Data);
+        Assert.IsFalse(result.Data.IsCorrect);
+
+        await _antifraudProducerMock.Received(1).ProduceAsync(Arg.Any<TransactionProcessedStatusDTO>());
+    }
+
+    [Test]
+    public async Task IsTransactionCorrect_TransactionRepositoryThrowsException_DoNotCallProducer()
+    {
+
+        _transactionRepositoryMock.GetSumOfTransactionsAsync(Arg.Any<Guid>())
+            .ThrowsAsync(new Exception("Exception"));
+
+        var result = await _antifraudService.IsTransactionCorrectAsync(_transactionToValidate);
+
+        Assert.IsNotNull(result);
+        Assert.IsFalse(result.IsSuccess);
+        Assert.IsNotNull(result.Error);
+        Assert.IsTrue(result.Error.Message.StartsWith("Unexpected error while trying to verify the transaction"));
+        Assert.That(result.Error.Code, Is.EqualTo(503));
+
+        await _antifraudProducerMock.DidNotReceive().ProduceAsync(Arg.Any<TransactionProcessedStatusDTO>());
+    }
+}
